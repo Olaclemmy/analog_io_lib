d4376c3c584c474fc418bb615b67e85d99105d2f
AnalogIO <lgbeno@analog.io>
2015-09-27 14:50:20 -0500

    Added BIT defines if not defined elsewhere (Arduino UNO)

--- nRF24L01.h
+++ nRF24L01.h
@@ -29,6 +29,17 @@
 #ifndef _NRF24L01_H
 #define _NRF24L01_H
 
+#ifndef BIT0
+    #define BIT0 0x01
+    #define BIT1 0x02
+    #define BIT2 0x04
+    #define BIT3 0x08
+    #define BIT4 0x10
+    #define BIT5 0x20
+    #define BIT6 0x40
+    #define BIT7 0x80
+#endif
+
 /* Register Map */
 #define RF24_CONFIG      0x00
 #define RF24_EN_AA       0x01
